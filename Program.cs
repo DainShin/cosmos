025b2f5498f0629403733195fad7b55a8c310428
DainShin <di.shin0611@gmail.com>
2024-09-01 18:51:13 +0900

    chnaged some variables for hosting

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Cosmos.Models;
 using Cosmos.Data;
 using Microsoft.Extensions.Options;
 using Cosmos.Services;
+using Stripe;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 // Add MySQL
-var connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException("Connection string not found");
+//var connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException("Connection string not found");
 
+var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING") ?? throw new InvalidOperationException("Connection string not found");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySQL(connectionString));
 
 // Redefining Folder Structure
@@ -41,10 +43,18 @@ builder.Services.AddDefaultIdentity<IdentityUser>()
 builder.Services.AddTransient<ApplicationDbInitializer>();
 
 // Registering Google Authentication
+//builder.Services.AddAuthentication().AddGoogle(options => {
+//	options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+//	options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+//});
 builder.Services.AddAuthentication().AddGoogle(options => {
-	options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-	options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+    options.ClientId = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_ID") ?? throw new InvalidOperationException("Google Client ID not found");
+    options.ClientSecret = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_SECRET") ?? throw new InvalidOperationException("Google Client Secret not found");
 });
+
+
+// Initialize Stripe
+StripeConfiguration.ApiKey = Environment.GetEnvironmentVariable("STRIPE_SECRET_KEY");
 
 // Register Cart Service as a new scoped dependency
 builder.Services.AddScoped<CartService>();
